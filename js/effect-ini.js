056b1a364782703c79ef3f94fb800607f590342b
David Bastos <davtechh@gmail.com>
2014-04-29 13:28:50 -0300

    Primeiro commit projeto sem alterações do original

---
+++ js/effect-ini.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+			$(document).ready(function() {
+				$("#newsletter").livingElements("images/input-mask.png", {
+				background:"url('images/gradient.png') no-repeat",
+				easing: 'swing',
+				preAnimationStartOpacity: 1,
+				mainAnimationFade: false,
+				scrollDirection: 'horizontal',					
+				mainAnimationDuration:3800,
+				mainAnimationStartBackgroundPositionX: -1300,     			
+     			mainAnimationEndBackgroundPositionX: 0,
+     			postAnimationEndOpacity: 0		
+				});
+				$(function () {
+				var austDay = new Date();
+				austDay = new Date(austDay.getFullYear() + 1, 6 - 11, 26);
+				$('#countdown').countdown({until: austDay, layout: 
+								'<div id="days" class="numbers">{dnn}</div>'+
+								'<div class="spacer"></div>'+
+								'<div id="hours" class="numbers">{hnn}</div>'+
+								'<div class="spacer"></div>'+
+								'<div id="minutes" class="numbers">{mnn}</div>'+
+								'<div class="spacer"></div>'+
+								'<div id="seconds" class="numbers">{snn}</div>'+
+								'</div>'});
+			});
+			});
